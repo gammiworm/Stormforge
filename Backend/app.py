05330da2ca30b75b6cdb250d12b971f9e7f220d6
Minseo Kim <charlottekimms@gmail.com>
2025-03-27 14:41:03 -0700

    Designed interpolation function

--- Backend/app.py
+++ Backend/app.py
@@ -6,6 +6,32 @@ def best_fit(x, y):
     m, b = np.polyfit(x, y, deg=1)
     return m, b
 
+def quadInterpolation():
+    x = []
+    y = []
+    xValue = fetch_x()
+    yValue = fetch_y()
+    size=len(xValue)
+    for row in range(size):
+        x.append(np.array(xValue[row][0]))
+        y.append(np.array(yValue[row][0]))
+
+    interpolated_points = []
+    m = (y[1] - y[0]) / (x[1] - x[0])
+    f = m(x-x[0]) + y[0]
+
+    for i in range(size - 1):
+        # if x[i] == x[i+1], then it is a vertical segment
+        if x[i] != x[i+1]:
+            a = (m*(x[i+1]-x[i])+(y[i+1]-y[i]))/(x[i+1]-x[i])**2
+            b = m - 2*a*x[i+1]
+            c = y[i+1] - a*x[i+1]**2 - m*x[i+1]
+            x_range = np.linspace(x[i], x[i+1], 50)
+            y_range = a*(x_range)**2 + b*(x_range) + c
+            interpolated_points.extend(zip(x_range, y_range))
+            m = 2*a*x[i+1] + b
+
+    return interpolated_points
 
 def main():
     x = []
